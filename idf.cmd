9bbc5aa9559f26ef941da0ff95cb9301fa0139b7
void-spark <81029971+void-spark@users.noreply.github.com>
2019-06-03 21:19:01 +0200

    Start using 'buildscripts'

--- idf.cmd
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-docker run -it --rm -v "%cd%:/esp/project" -w /esp/project esp32-idf bash
--- make_clean.cmd
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-docker run -it --rm -v "%cd%:/esp/project" -w /esp/project esp32-idf bash -c "rm -rf build && rm sdkconfig && make defconfig && make -j8 clean"
